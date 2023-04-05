1278b3b7588c6567610494c08cafbd990fbae57c
ghassenbenghorbal <ghassen.benghorbal99@gmail.com>
2023-04-05 22:32:20 +0100

    real time chat done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,8 +11,16 @@ class HomeController extends Controller
     public function index()
     {
         $patients = Patient::all();
+        $heartRateImage = asset(\Storage::url('images/heart-rate.jpg'));
+        $temperatureImage = asset(\Storage::url('images/temperature.jpg'));
+        $bloodPressureImage = asset(\Storage::url('images/blood-pressure.png'));
+        $overallImage = asset(\Storage::url('images/overall.png'));
         return Inertia::render('home', [
             'patients_' => $patients,
+            'heartRateImage' => $heartRateImage,
+            'temperatureImage' => $temperatureImage,
+            'bloodPressureImage' => $bloodPressureImage,
+            'overallImage' => $overallImage,
         ]);
     }
 }
